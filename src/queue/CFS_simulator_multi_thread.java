9f746923999d81a7a0af5d7991b3f629df6b0d3b
JackChuang <laborjack@gmail.com>
2016-12-04 23:29:04 -0500

    File /Users/jack/Dropbox/06MPP/Project/code/CFSLockFreeRBTree/src/queue/CFS_simulator_multi_thread.java changed at Sun Dec 04 23:29:03 EST 2016

--- src/queue/CFS_simulator_multi_thread.java
+++ src/queue/CFS_simulator_multi_thread.java
@@ -537,7 +537,7 @@ if(DEBUG){
 			  			/* clean runtime info to record for the next run */
 			  			//curr_task.cpu_runtime=0;
 			  			//curr_task.io_runtime=0;
-			  			System.out.println("1. " + curr_ta)
+			  			System.out.println("1. ");
 			  			kill_from_rbtree(curr_task, instance, _lock);
 			  			System.out.println(((AVL<Task>)instance).height());
 					}
