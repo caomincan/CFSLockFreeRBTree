34d4666b3f855c41d71ed1dc414fff78f37193b7
JackChuang <laborjack@gmail.com>
2016-12-05 00:36:59 -0500

    File /Users/jack/Dropbox/06MPP/Project/code/CFSLockFreeRBTree/src/queue/CFS_simulator_multi_thread.java changed at Mon Dec 05 00:36:59 EST 2016

--- src/queue/CFS_simulator_multi_thread.java
+++ src/queue/CFS_simulator_multi_thread.java
@@ -574,7 +574,7 @@ if(DD) {
 						t_time++;
 						is_exit = JobTask(curr_task, 0); 
 					//if (t_time > TimerIntThreshold || is_exit==true) // case1 + case2
-					if (t_time > TimerIntThreshold || is_exit==true) // case1 + case2
+					if (t_time > 10000000000000 || is_exit==true) // case1 + case2
 
 						break;
 				}while(true);
