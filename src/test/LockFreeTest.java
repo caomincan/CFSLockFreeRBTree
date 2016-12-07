5ebd37fecdeaa3ce63e7e6128baaa96a0f481c74
caomincan <micao@vt.edu>
2016-12-06 20:34:21 -0500

    add search test

--- src/test/LockFreeTest.java
+++ src/test/LockFreeTest.java
@@ -7,7 +7,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import tree.*;
 
-public class Test {
+public class LockFreeTest {
 
 	public static void main(String[] args) throws InterruptedException {
 	   long start= 0,duration = 0;
@@ -22,46 +22,66 @@ public class Test {
     	   }
        }
 		// TODO Auto-generated method stub
-       Tree<Integer> tree = null;
+       // Adding test
+       System.out.println("Lock-Free Red-Black Tree Adding test");
        List<Thread> threads = new ArrayList<Thread>();
        Lock mylock = new ReentrantLock();
        
        threads.clear();
-       tree = new RBTree<Integer>();    
+       Tree<Integer> tree1 = new RBTree<Integer>();    
        for(int i = 0;i<num_threads;i++){
     	   Integer[] list = new Integer[insert_nodes];
     	   for(int j = 0;j<insert_nodes;j++){
     		   list[j] = insert_nodes*i+j;
             }
         //if(i == 0) threads.add(new searchThread(tree,num_threads));
-    	   threads.add(new testThread<Integer>(tree,list,mylock));
+    	   threads.add(new testThread<Integer>(tree1,list,mylock));
         }
        start = System.nanoTime(); 
        for(Thread thread: threads) thread.start();
        for(Thread thread: threads) thread.join();
        duration = System.nanoTime() - start;
        System.out.println("RBTree each thread insert "+insert_nodes+" nodes using " +(double)duration/1000.0 + " us");
-       tree.print();
-       System.out.println("");
-       
        
        threads.clear();
-       tree = new LockFreeRBTree<Integer>();
+       Tree<Integer> tree2 = new LockFreeRBTree<Integer>();
        for(int i = 0;i<num_threads;i++){
     	   Integer[] list = new Integer[insert_nodes];
     	   for(int j = 0;j<insert_nodes;j++){
     		   list[j] = insert_nodes*i+j;
             }
         //if(i == 0) threads.add(new searchThread(tree,num_threads));
-    	   threads.add(new testThread<Integer>(tree,list));
+    	   threads.add(new testThread<Integer>(tree2,list));
         }
        start = System.nanoTime(); 
        for(Thread thread: threads) thread.start();
        for(Thread thread: threads) thread.join();
        duration = System.nanoTime() - start;
        System.out.println("LockFreeRBTree each thread insert "+insert_nodes+" nodes using " +(double)duration/1000.0 + " us");
-       tree.print();
-       System.out.println("");
+       
+       System.out.println("\n\rLock-Free Red-Black Tree Search test");
+       //tree2.print();
+       //System.out.println("");
+       
+       threads.clear();
+       for(int i = 0;i<num_threads;i++){
+    	   threads.add(new searchThread(tree1,num_threads,insert_nodes,mylock));
+        }
+       start = System.nanoTime(); 
+       for(Thread thread: threads) thread.start();
+       for(Thread thread: threads) thread.join();
+       duration = System.nanoTime() - start;
+       System.out.println("RBTree each thread search 1000 times using " +(double)duration/1000.0 + " us");
+       
+       threads.clear();
+       for(int i = 0;i<num_threads;i++){
+    	   threads.add(new searchThread(tree2,num_threads,insert_nodes));
+        }
+       start = System.nanoTime(); 
+       for(Thread thread: threads) thread.start();
+       for(Thread thread: threads) thread.join();
+       duration = System.nanoTime() - start;
+       System.out.println("LockFreeRBTree each thread search 1000 times using " +(double)duration/1000.0 + " us");
 	}
 
 }
--- src/test/searchThread.java
+++ src/test/searchThread.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
 
 import tree.Tree;
 
@@ -10,25 +11,39 @@ public class searchThread extends Thread{
 	public int id;
 	private Tree<Integer> tree;
 	private int num;
+	private int nodes;
+	private Lock lock = null;
 	
-	public searchThread(Tree<Integer> tree,int num){
+	public searchThread(Tree<Integer> tree,int num,int nodes){
 		this.id = ID_GEN++;
 		this.tree = tree;
 		this.num = num;
+		this.nodes = nodes;
+		this.lock = null;
+	}
+	
+	public searchThread(Tree<Integer> tree,int num,int nodes,Lock lock){
+		this.id = ID_GEN++;
+		this.tree = tree;
+		this.num = num;
+		this.nodes = nodes;
+		this.lock = lock;
 	}
 	
 	@Override
 	public void run(){
 		Random rand = new Random();
-		for(int i=0;i<100;i++){
-			Integer target = rand.nextInt(20*num);
+		for(int i=0;i<1000;i++){
+			if(lock != null) lock.lock();
+			Integer target = rand.nextInt(nodes*num);
 			//System.out.println("Thread "+id+" search "+target);
 			Integer result = this.tree.search(target);
-			if(result == null) System.out.println("Search fail:  "+target);
-			else System.out.println("Search found: "+result);
+			if(lock != null) lock.unlock();
+			//if(result == null) System.out.println("Search fail:  "+target);
+			//else System.out.println("Search found: "+result);
 			//this.tree.print();
 		}
-		System.out.println("Thread "+id+": finished");
+		//System.out.println("Thread "+id+": finished");
 		//this.tree.print();
 		//System.out.println("");
 	}
